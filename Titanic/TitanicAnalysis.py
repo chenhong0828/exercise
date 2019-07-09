dfbcb591ae58c62191197e4c1b28ee4adec856c3
chenhong0828 <chenhong0828@google.com>
2019-07-09 18:15:44 +0800

    no message

--- Titanic/TitanicAnalysis.py
+++ Titanic/TitanicAnalysis.py
@@ -212,15 +212,15 @@ predictions = clf.predict(test)
 
 # print(pd.DataFrame({'columns': list(train_df.columns)[1:], 'coef': list(clf.coef_.T)}))
 
-from sklearn.model_selection import train_test_split
-from sklearn import cross_validation
-from sklearn.learning_curve import learning_curve 
+# from sklearn.model_selection import train_test_split
+# from sklearn.model_selection import cross_val_score
+# from sklearn.model_selection import learning_curve 
 
-clf = linear_model.LogisticRegression(C=1.0, penalty='l1', tol=1e-6)
-all_data = df.filter(regex='Survived|Age_.*|SibSp|Prach|Fare_.*|Cabin_.*|Embarked_.*|Sex_.*|Pclass_.*')
-X = all_data.values[:, 1:]
-y = all_data.values[:, 0]
-print(cross_validation.cross_val_score(clf, X, y, cv=5))
+# clf = linear_model.LogisticRegression(C=1.0, penalty='l1', tol=1e-6)
+# all_data = df.filter(regex='Survived|Age_.*|SibSp|Prach|Fare_.*|Cabin_.*|Embarked_.*|Sex_.*|Pclass_.*')
+# X = all_data.values[:, 1:]
+# y = all_data.values[:, 0]
+# print(cross_val_score(clf, X, y, cv=5))
 
 # split_train, split_cv = train_test_split(df, test_size=0.3, random_state=0)
 # train_df = split_train.filter(regex='Survived|Age_.*|SibSp|Prach|Fare_.*|Cabin_.*|Embarked_.*|Sex_.*|Pclass_.*')
@@ -231,3 +231,53 @@ print(cross_validation.cross_val_score(clf, X, y, cv=5))
 # origin_data_train = pd.read_csv('E:/dataAnalysis/Titanic/train.csv', engine="python")
 # bad_cases = origin_data_train.loc[origin_data_train['PassengerId'].isin(split_cv[predictions != cv_df.values[:, 0]]['PassengerId'].values)]
 # print(bad_cases)
+
+# def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None, n_jobs=1,
+#                         train_sizes = np.linspace(.05, 1., 20), verbose=0, plot=True):
+#     train_sizes, train_scores, test_scores = learning_curve(
+#         estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes, verbose=verbose)
+    
+#     train_scores_mean = np.mean(train_scores, axis=1)
+#     train_scores_std = np.std(train_scores, axis=1)
+#     test_scores_mean = np.mean(test_scores, axis=1)
+#     test_scores_std = np.std(test_scores, axis=1)
+
+#     if plot:
+#         plt.figure()
+#         plt.title(title)
+#         if ylim is not None:
+#             plt.ylim(*ylim)
+#         plt.xlabel(u'训练样本数')
+#         plt.ylabel(u'得分')
+#         # plt.gca().invert_yaxis()
+#         plt.grid()
+
+#         plt.fill_between(train_sizes, train_scores_mean - train_scores_std, train_scores_mean + train_scores_std,
+#                         alpha=0.1, color='b')
+#         plt.fill_between(train_sizes, test_scores_mean - test_scores_std, test_scores_mean + test_scores_std,
+#                         alpha=0.1, color='r')
+#         plt.plot(train_sizes, train_scores_mean, 'o-', color='b', label=u'训练集上得分')
+#         plt.plot(train_sizes, test_scores_mean, 'o-', color='r', label=u'交叉验证集上得分')
+
+#         plt.legend(loc='best')
+
+#         plt.draw()
+#         plt.show()
+#         # plt.gca().invert_yaxis()
+
+#     midpoint = ((train_scores_mean[-1] + train_scores_std[-1]) + (test_scores_mean[-1] - test_scores_std[-1])) / 2
+#     diff = (train_scores_mean[-1] + train_scores_std[-1]) - (test_scores_mean[-1] - test_scores_std[-1])
+#     print(midpoint, diff)
+# plot_learning_curve(clf, u'学习曲线', X, y, ylim = (0.75,0.9))
+
+from sklearn.ensemble import BaggingRegressor
+train_df = df.filter(regex='Survived|Age_.*|SibSp|Parch|Fare_.*|Cabin_.*|Embarked_.*|Sex_.*|Pclass_.*')
+train_np = train_df.values
+
+y = train_np[:, 0]
+X = train_np[:, 1:]
+
+clf = linear_model.LogisticRegression(C=1.0, penalty='l1', tol=1e-6)
+bagging_clf = BaggingRegressor(clf, n_estimators=20, max_samples=0.8, max_features=1.0, bootstrap=True, bootstrap_features=False, n_jobs=-1)
+
+clf.fit(X, y)
