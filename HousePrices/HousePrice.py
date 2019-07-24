6d9519fe96953b1060cca85c93bf114afcd72d09
chenhong0828 <chenhong0828@google.com>
2019-07-24 18:04:18 +0800

    no message

--- HousePrices/HousePrice.py
+++ HousePrices/HousePrice.py
@@ -2,9 +2,13 @@ import numpy as np
 import pandas as pd
 
 train_data = pd.read_csv('./train.csv')
+test_data = pd.read_csv('./test.csv')
 pd.set_option('max_columns', 100)
 # print(train_data.shape)
-# print(train_data.isnull().sum().sort_values(ascending=False))
-# print(train_data['PoolQC'].value_counts(dropna=False))
-# print(train_data['LotFrontage'].max())
-print(train_data.describe())
+# print(test_data.shape)
+full_data = pd.concat([train_data, test_data], ignore_index=True)
+print(full_data.shape)
+temp = full_data.isnull().sum()
+print(temp[temp>0].sort_values(ascending=False))
+# print(full_data['LotFrontage'].max())
+# print(full_data.describe())
