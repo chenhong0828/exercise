5ec322b48ca49aaf2f354d7242879b45d4dbc955
honghong <chenhong0828@gmail.com>
2019-07-24 23:41:47 +0800

    HousePrice

--- HousePrices/HousePrice.py
+++ HousePrices/HousePrice.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from matplotlib import pyplot as plt 
 
 train_data = pd.read_csv('./train.csv')
 test_data = pd.read_csv('./test.csv')
@@ -10,5 +11,4 @@ full_data = pd.concat([train_data, test_data], ignore_index=True)
 print(full_data.shape)
 temp = full_data.isnull().sum()
 print(temp[temp>0].sort_values(ascending=False))
-# print(full_data['LotFrontage'].max())
 # print(full_data.describe())
